c5d4a792cd3f96e2e7ad63d523b6eec24c6df628
ZeeshanAyub01 <43322038+ZeeshanAyub01@users.noreply.github.com>
2020-02-02 02:31:28 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 participantList = [];
 temp_list = [];
+transactions_list = [];
 var total_particpants = participantList.length;
 
 class Participant {
@@ -95,6 +96,7 @@ var transaction_add_link = document.getElementById('transaction_add_link');
 var owed_amount = document.getElementById('amount');
 var amount_currency = document.getElementById('currency');
 var transaction_desc = document.getElementById('transaction_desc');
+var done_adding_trans_btn = document.getElementById('done_adding_trans_btn');
 
 
 add_btn.addEventListener("click", function(){
@@ -184,8 +186,8 @@ transaction_add_btn.addEventListener("click", function () {
     participant_creditor.credits.push(newTransaction);
     participant_debtor.debts.push(newTransaction);
 
-    console.log(participant_creditor.name + ' is owed a total of: ' + currency + participant_creditor.total_credit + '. Thus their net credit = ' + participant_creditor.net_credit);
-    console.log(participant_debtor.name + ' owes a total of: ' + currency + participant_debtor.total_debt + '. Thus their net credit = ' + participant_debtor.net_credit);
+    //console.log(participant_creditor.name + ' is owed a total of: ' + currency + participant_creditor.total_credit + '. Thus their net credit = ' + participant_creditor.net_credit);
+    //console.log(participant_debtor.name + ' owes a total of: ' + currency + participant_debtor.total_debt + '. Thus their net credit = ' + participant_debtor.net_credit);
 
     added_participants_2.value = "";
     added_participants_1.value = "";
@@ -206,6 +208,90 @@ function fetchParticipant(participant_name){
     return null;
 }
 
-function compare_net_credits(participant_1, participant_2){
-    return participant_1.net_credit - participant_2.net_credit;
+function addUnresolvedAmount() {
+    shortList = [];
+    j = 0;
+    for (var i = 0; i < participantList.length; i++) {
+        if (participantList[i].net_credit != 0) {
+            shortList.push(participantList[i]);
+            shortList[j].unresolved_amount = participantList[i].net_credit;
+            j += 1;
+        }
+    }
+
+    return shortList;
 }
+
+function generateShorterList(initShortList){
+    shorterList = [];
+    
+    for (var i = 0; i < initShortList.length; i++){
+        if (initShortList[i].unresolved_amount != 0){
+            shorterList.push(initShortList[i]);
+        }
+        
+    }
+
+    initShortList = [];
+    return shorterList;
+}
+
+function resolveADebt(initShortList){
+
+    initShortList.sort(function (p1, p2) {
+        return p1.unresolved_amount - p2.unresolved_amount;
+    });
+
+    p1 = initShortList[0];
+    p2 = initShortList[initShortList.length - 1];
+
+    ura_1 = p1.unresolved_amount;
+    ura_2 = p2.unresolved_amount;
+
+    //console.log('The UR of ' + p1.name + ' is: ' + p1.unresolved_amount + ' and the UR of ' + p2.name + ' is: ' + p2.unresolved_amount);
+
+    if (Math.abs(p1.unresolved_amount) > p2.unresolved_amount){
+        console.log(p1.name + ' should pay ' + p2.name + ' $' + Math.abs(p2.unresolved_amount));
+        initShortList[0].unresolved_amount += Math.abs(initShortList[initShortList.length - 1].unresolved_amount);
+        initShortList[initShortList.length - 1].unresolved_amount -= Math.abs(ura_2);
+        // console.log('Now ' + p1.name + '\'s unresolved amount is = ' + p1.unresolved_amount);
+        // console.log('And ' + p2.name + '\'s unresolved amount is = ' + p2.unresolved_amount);
+    }
+    else if (Math.abs(p1.unresolved_amount) <= p2.unresolved_amount){
+        console.log(p1.name + ' should pay ' + p2.name + ' $' + Math.abs(p1.unresolved_amount));
+        initShortList[0].unresolved_amount += Math.abs(initShortList[0].unresolved_amount);
+        initShortList[initShortList.length - 1].unresolved_amount -= Math.abs(ura_1);
+        // console.log('Now ' + p1.name + '\'s unresolved amount is = ' + p1.unresolved_amount);
+        // console.log('And ' + p2.name + '\'s unresolved amount is = ' + p2.unresolved_amount);
+    }
+
+    return initShortList;
+
+}
+
+
+done_adding_trans_btn.addEventListener("click", function () {
+
+    shortList = addUnresolvedAmount();//To add an extra attribute 'unresolved_amount' to participants whose net_credit is non-zero; called only once
+    
+
+    // let sorted_low_to_high = [];
+    // let sorted_high_to_low = [];
+
+    while (shortList.length > 1){
+        shortList = resolveADebt(shortList);
+        shortList = generateShorterList(shortList);//To shorten the list based on participants whose unresolved_amount is still non-zero; called multiple times
+    }
+
+    // sorted_low_to_high.sort(function (p1, p2){
+    //     return p1.net_credit - p2.net_credit;
+    // });
+
+    // sorted_high_to_low.sort(function (p1, p2) {
+    //     return p2.net_credit - p1.net_credit;
+    // });
+
+    // console.log(sorted_low_to_high);
+    // console.log('***************************************');
+    // console.log(sorted_high_to_low);
+});
